3ada0329bc1876c8a4d394f0fb2d73f6acb1a5ab
dbarthelmeh <dcbarthelmeh@gmail.com>
2020-11-24 14:29:09 -0700

    Cleaned up the coherence of outputs.

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ class SLL(object):
                         trav.next.previous = trav.previous
                 else:
                     self.head = trav.next  # reassign the head
-                    self.head.previous = None  # head has no previous
+                    if self.head is not None:
+                        self.head.previous = None  # head has no previous
                 del trav
                 return print('Deleted', targeted_data)
 
@@ -30,6 +31,9 @@ class SLL(object):
                 trav = trav.next
 
     def insert(self, node_data):
+        if self.head is None:
+            self.head = Node(node_data)
+            return
         trav = self.head
         while trav.next is not None:
             trav = trav.next
@@ -42,14 +46,15 @@ class SLL(object):
         while trav is not None:
             p = p + 1
             if trav.data == node_data:
-                return print(node_data, 'is in position', p)
+                return print('Search found that', node_data, 'is in position', p)
             trav = trav.next
         return print(node_data, 'was not found')
 
     def show(self):
         trav = self.head
+        print('List is now ', end='')  # no new lines are printed
         if trav is None:
-            return print('This list is so empty.')
+            return print('so empty')
         while trav.next is not None:
             print(trav.data, '- ', end='')  # no new lines are printed
             trav = trav.next
@@ -58,14 +63,14 @@ class SLL(object):
     def sort(self, order='descending'):
         """Set direction to ascending to get ascending order and to descending to get descending order."""
         if order == 'descending':
-            order = '<'
+            symbol = '<'
         elif order == 'ascending':
-            order = '>'
+            symbol = '>'
         swap_needed = True
         while swap_needed:
             trav = self.head.next
             while trav is not None:
-                if eval(str(trav.previous.data) + order + str(trav.data)):
+                if eval(str(trav.previous.data) + symbol + str(trav.data)):
                     temp = trav.data
                     trav.data = trav.previous.data
                     trav.previous.data = temp
@@ -74,6 +79,7 @@ class SLL(object):
                     trav = trav.next
             if trav is None:  # we've completed the previous loop with no swaps
                 swap_needed = False
+            return print('Sorted', order)
 
 
 llist = SLL()
@@ -83,6 +89,12 @@ llist.insert(7)
 llist.show()
 llist.search(3)
 llist.delete(7)
+llist.show()
+llist.delete(3)
+llist.delete(8)
+llist.show()
+llist.insert(8)
+llist.insert(3)
 llist.show()
 llist.search(10)
 llist.insert(7)
